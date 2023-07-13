4abd46539359738041460cbe02e772a259a57422
mkreive <mkreive@gmail.com>
2023-07-13 11:25:11 +0300

    initial

---
+++ src/main/java/lt/monikos/bookeeplibrary/BookeepLibraryApplication.java
@@ -0,0 +1,13 @@
+package lt.monikos.bookeeplibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookeepLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookeepLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/monikos/bookeeplibrary/BookeepLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.monikos.bookeeplibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookeepLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
