73c64df93debbdb63273a2ba824cb1845785b28f
mkreive <mkreive@gmail.com>
2023-07-18 10:18:58 +0300

    search by title

--- src/main/java/lt/monikos/bookeeplibrary/repository/BookRepository.java
+++ src/main/java/lt/monikos/bookeeplibrary/repository/BookRepository.java
@@ -1,10 +1,14 @@
 package lt.monikos.bookeeplibrary.repository;
 
 import lt.monikos.bookeeplibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    Optional<Book> findById(int id);
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+
 }
