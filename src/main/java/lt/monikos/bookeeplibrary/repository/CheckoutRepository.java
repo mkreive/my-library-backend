08995c0cf1bb0036d7043c3c56ed45d3fc955edb
mkreive <mkreive@gmail.com>
2023-07-19 11:26:17 +0300

    checkout entity

---
+++ src/main/java/lt/monikos/bookeeplibrary/repository/CheckoutRepository.java
@@ -0,0 +1,9 @@
+package lt.monikos.bookeeplibrary.repository;
+
+import lt.monikos.bookeeplibrary.entity.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+
+    Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+}
