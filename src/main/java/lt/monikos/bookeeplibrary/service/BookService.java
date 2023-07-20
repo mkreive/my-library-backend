13805c4eabe1f3ad0a395ee8d80e4869fdf5fdd1
mkreive <mkreive@gmail.com>
2023-07-20 15:27:12 +0300

    returning a book functionality

--- src/main/java/lt/monikos/bookeeplibrary/service/BookService.java
+++ src/main/java/lt/monikos/bookeeplibrary/service/BookService.java
@@ -91,8 +91,22 @@ public class BookService {
                 shelfCurrentLoansResponses.add(new ShelfCurrentLoansResponse(book, (int) difference_In_Time));
             }
         }
-
         return shelfCurrentLoansResponses;
+    }
+
+    public void returnBook(String userEmail, Long bookId) throws Exception {
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if(!book.isPresent() || validateCheckout == null) {
+            throw new Exception("book does not exist or not checked out by user");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
+        bookRepository.save(book.get());
+
+        checkoutRepository.deleteById(validateCheckout.getId());
     }
 
 
